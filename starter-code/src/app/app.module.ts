10824de1746e4067f5b0d4b690daeeed7954d3a7
Will Schaller <willschaller@gmail.com>
2018-07-29 17:10:55 +0200

    Add routes

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { MovieDisplayComponent } from './components/movie-display/movie-display.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MoviesHomeComponent }
+  // { path: 'movie/:id', component: MovieDisplayComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MoviesHomeComponent, MovieDisplayComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
